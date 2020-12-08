6c0203b24f86f057c2d043587dd9afc9ff6e6c05
Ethan Wu <ewu000@berkeley.edu>
2020-12-08 03:13:49 -0600

    def fucked up somewhere

--- frontend/spotify-webapp/src/pages/Loading.js
+++ frontend/spotify-webapp/src/pages/Loading.js
@@ -8,6 +8,12 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import LandingLogo from '../img/logo.png';
+import LandingDescriptionPhoto from '../img/landing-description-photo.png';
+import MetaImage from '../img/meta-image.png';
+
+import '../css/Landing.css';
+
 const Landing = () => {
     $(document).ready(function () {
         /* Check the location of each desired element */
@@ -34,19 +40,96 @@ const Landing = () => {
             });
         });
     });
-<nav class="navbar navbar-expand-lg fixed-top ">
-    <a class="navbar-brand" href="#"></a>
-    <img src="images/wdb-black+white.png" class="img-fluid, logo" alt="hello">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-4">
-                <li class="nav-item">
-                    <a class="nav-link, top" data-value="about" href="#">About</a></li>
-                <div class="vl"></div>
-                <li class="nav-item">
-                    <a class="nav-link, top" data-value="portfolio" href="#">Log In</a>
-                </li>
-            </ul>
-        </div></nav>
+    return (
+        <div className="landing">
+            <SiteNavbar sticky="false" bg="transparent" />
+            {/* Keep This image here for site preview */}
+            <img
+                src={MetaImage}
+                alt="meta"
+                className="selectDisable"
+                style={{ display: 'none' }}
+            />
+            <div className="header">
+                <img
+                    src={LandingLogo}
+                    alt="landing logo"
+                    className="landing-logo selectDisable"
+                />
+                <h2>UC Berkeley's Formula SAE Electric Vehicle Team</h2>
+                <a href="/about">
+                    <Button className="button landing-button">
+                        <p className="button-text">Learn More</p>
+                    </Button>
+                </a>
+            </div>
+            <div className= "recruitvideo">
+                <h4>Crowdfunding</h4>
+                <hr2/>
+                <Row noGutters="true">
+                    <div className= "video">
+                        <iframe
+                            class="responsive-iframe"
+                            height="380"
+                            width="660"
+                            src="https://www.youtube.com/embed/TouP6oxgqZQ"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; encrypted-media;
+				gyroscope; picture-in-picture" allowfullscreen>
+                        </iframe>
+                    </div>
+                </Row>
+                <div>
+                    <br/>
+                    <br/>
+                    <a href= "https://crowdfund.berkeley.edu/project/20151">
+                        <Button className="button join-button fade-in">
+                            <p className="button-text">Donate Here!</p>
+                        </Button>
+                    </a>
+                </div>
+            </div>
+            <Container fluid className="body">
+                <Row noGutters="true">
+                    <Col xs={12} sm={6} className="text-col">
+                        <h3>Our Mission</h3>
+                        <hr />
+                        <div className="text-container">
+                            <p className="text fade-in">
+                                Create a{' '}
+                                <b className="yellow">community of diverse individuals</b> that
+                                focuses on learning and skill development.
+                            </p>
+                            <p className="text fade-in">
+                                Research, design, manufacture, and test{' '}
+                                <b className="blue">
+                                    efficient, sustainable transportation technology
+                                </b>{' '}
+                                at UC Berkeley.
+                            </p>
+                            <p className="text fade-in">
+                                Compete in the annual{' '}
+                                <b className="yellow">Formula SAE Electric Competition</b> to
+                                demonstrate our technology at a global scale.
+                            </p>
+                        </div>
+                        <a href="/join">
+                            <Button className="button join-button fade-in">
+                                <p className="button-text">Join The Team</p>
+                            </Button>
+                        </a>
+                    </Col>
+                    <Col xs={12} sm={6} className="text-center">
+                        <img
+                            src={LandingDescriptionPhoto}
+                            alt="landing description"
+                            className="text-image selectDisable"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Landing;
